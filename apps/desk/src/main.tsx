0480a725af78e9185126af5f71f63f6e12f65492
RedRobot <git@redrobot.jp>
2026-07-22 09:09:12 -0700

    chore(desk): import intent-log styles

--- apps/desk/src/main.tsx
+++ apps/desk/src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import "./styles.css";
 import "./flow-rail.css";
 import "./styles-chain-patch.css";
+import "./styles-intent-log.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
