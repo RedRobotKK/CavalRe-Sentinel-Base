1d914572aa87d2a80dd1759c2f6ddc2812c9930a
RedRobot <git@redrobot.jp>
2026-07-22 10:42:16 -0700

    chore(wallet): export lifecycle API

--- packages/wallet/src/index.ts
+++ packages/wallet/src/index.ts
@@ -4,3 +4,13 @@ export { publicKeyToAddress, assertAddress } from "./address.js";
 export { ethPath, assertEthPath, ETH_PATH_PREFIX } from "./derivation.js";
 export { LocalSigner, ethMessageHash } from "./signer.js";
 export { encodeBalanceOf, encodeTransfer, encodeApprove } from "./erc20.js";
+export {
+  resolveWalletSession,
+  walletPublicContext,
+  assertCanSign,
+  WALLET_USE_CASES,
+  type WalletPosture,
+  type IntegrationPhaseId,
+  type WalletSession,
+  type WalletLifecycleInput,
+} from "./lifecycle.js";
